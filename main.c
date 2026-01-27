d9a4cf0015827b7b35db1ddbaa179a7a4b02a691
lakshmanarayananjb <lakshmanarayananjb@yantravision.com>
2026-01-27 11:58:25 +0530

    added the coorect code

--- main.c
+++ main.c
@@ -1,19 +1,34 @@
 
+
+#include <stdio.h>
+
 void checkNumber(int n) {
     int i, isPrime = 1;
 
-    if (n <= 1)
+    if (n <= 1) {
         isPrime = 0;
-
-    for (i = 2; i < n; i++) {
-        if (n % i == 0) {
-            isPrime = 0;
-            break;
+    } else {
+        for (i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                isPrime = 0;
+                break;
+            }
         }
     }
 
     if (isPrime)
-        printf("Prime number\n");
+        printf("%d is a Prime number\n", n);
     else
-        printf("Not a prime number\n");
+        printf("%d is Not a Prime number\n", n);
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    checkNumber(num);
+
+    return 0;
 }
